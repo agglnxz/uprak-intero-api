3b8228f411c884a6bd553c9ffd039453f1ca52b4
inandiarsharfinafauzi <inandiarsharfinafauzi02@gmail.com>
2025-12-12 16:48:53 +0700

    Update_VendorC_Mahasiswa3

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ const app = express();
 app.use(express.json());
 
 // ROUTES
-// const vendorARoute = require("./src/routes/vendorA.route");
-const vendorBRoute = require("./src/routes/vendorB.route");
+const vendorRoute = require("./src/routes/route");
 
-// app.use("/vendorA", vendorARoute);
-app.use("/vendorB", vendorBRoute);
-
+app.use("/api", vendorRoute); 
 // run server
 app.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
