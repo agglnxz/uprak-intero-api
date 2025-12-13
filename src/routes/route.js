3c95e96f26f22d72f53f8e7086b40a97a3bc8270
yossyfira8-ship-it <yossyfira8@gmail.com>
2025-12-13 11:38:38 +0700

    Mahasiswa1:VendorA

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,18 @@
-// src/routes/vendorB.route.js
-
 const express = require("express");
 const router = express.Router();
+
+const vendorAData = require("../vendors/vendorA");
 const vendorBData = require("../vendors/vendorB");
 const vendorCData = require("../vendors/vendorC");
+
+// vendor A
+router.get("/vendorA", (req, res) => {
+  res.json({
+    status: "success",
+    message: "Data Vendor A retrieved successfully",
+    data: vendorAData
+  });
+});
 
 // Endpoint untuk menampilkan data vendor B
 router.get("/vendorB", (req, res) => {
@@ -13,6 +22,7 @@ router.get("/vendorB", (req, res) => {
     data: vendorBData
   });
 });
+
 router.get("/vendorC", (req, res) => {
   res.json({
     status: "success",
---
+++ src/vendors/vendorA.js
@@ -0,0 +1,34 @@
+const vendorAData = [
+  {
+    "kd_produk": "A001",
+    "nm_brg": "Kopi Bubuk 100g",
+    "hrg": "15000",
+    "ket_stok": "ada"
+  },
+  {
+    "kd_produk": "A002",
+    "nm_brg": "Gula Pasir 1kg",
+    "hrg": "14000",
+    "ket_stok": "habis"
+  },
+  {
+    "kd_produk": "A003",
+    "nm_brg": "Minyak Goreng 1L",
+    "hrg": "21000",
+    "ket_stok": "ada"
+  },
+  {
+    "kd_produk": "A004",
+    "nm_brg": "Beras Medium 5kg",
+    "hrg": "52000",
+    "ket_stok": "ada"
+  },
+  {
+    "kd_produk": "A005",
+    "nm_brg": "Mie Goreng 80g",
+    "hrg": "2500",
+    "ket_stok": "habis"
+  }
+];
+
+module.exports = vendorAData;
